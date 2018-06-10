3544fcbf4a78004a76793cd3266025317f0bacfb
Konrad Stalęga <konradstalega@gmail.com>
2018-06-10 15:36:42 +0200

    FIxing recursivness

--- src/Board.hs
+++ src/Board.hs
@@ -5,13 +5,13 @@ import DataStructures
 -- printing board based on input date
 printBoard :: State -> Int -> Int -> IO ()
 printBoard state i j  | j < 8 = do 
-                                    putStrLn ("|" ++ (printRow state i j) ++ "|")
+                                    putStrLn ((printRow state i j) ++ "|")
                                     printBoard state 0 (j+1)
                       | otherwise = putStrLn ""
 
 -- printing one row of board
 printRow :: State -> Int -> Int -> String
-printRow state i j | i < 8 = (printField state i j) ++ (printField state (i+1) j)
+printRow state i j | i < 8 = "|" ++ (printField state i j) ++ (printRow state (i+1) j)
                    | otherwise = ""
 
 -- printing field of board
