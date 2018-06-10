e72ef7db5823bc492e5a5fc7e38004bd6040b697
Konrad Stalęga <konradstalega@gmail.com>
2018-06-10 19:38:19 +0200

    Better parsing of arguments for sheep moving.

--- src/Menu.hs
+++ src/Menu.hs
@@ -1,20 +1,35 @@
 module Menu where
 
+import Data.Char
 import DataStructures
+import Board
 
+-- process comands during the game
 processComands :: State -> IO()
 processComands state = do
                   (operationID:commandSettings) <- fmap words getLine
                   case operationID of
                     '-':'m':_ -> do
                                   processSheepSteering state commandSettings
-                                  putStrLn "Moving ship"
+                                  putStrLn "Moving sheep"
+                                  printBoard state 0 0
                                   processComands state
                     _ -> do
                           putStrLn "Wrong command"
                           processComands state
 
+-- process sheep steering command
 processSheepSteering :: State -> [String] -> IO()
-processSheepSteering state (sheepIndex:directionOfMovement:_)  = do
-                                                    putStrLn sheepIndex
-                                                    putStrLn directionOfMovement
+processSheepSteering state ((sheepIndex:_):(directionOfMovement:_):_)  = do
+                                                                          steerSheep (getSheepIndexAsAInt sheepIndex) directionOfMovement
+
+-- get sheep index as int
+getSheepIndexAsAInt :: Char -> Int
+getSheepIndexAsAInt x | isDigit x == True = digitToInt x
+                      | otherwise = error "Index of sheep wrongly formattted"
+
+-- steer sheep
+steerSheep :: Int -> Char -> IO()
+steerSheep sheepIndex directionOfMovement = do
+                                              putStrLn (show sheepIndex)
+                                              putStrLn [directionOfMovement]
